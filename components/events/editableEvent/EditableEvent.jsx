1a503f55cca7b3d30879a87a0850b1f63dda350a
fedorareis <fedorareis@gmail.com>
2019-07-30 23:38:43 -0700

    display dimple form dialog

--- components/events/editableEvent/EditableEvent.jsx
+++ components/events/editableEvent/EditableEvent.jsx
@@ -1,214 +1,179 @@
 import * as React from 'react';
+import PropTypes from 'prop-types';
 import * as moment from 'moment';
-import { withStyles } from '@material-ui/core/styles';
-import { Dialog, DialogActions, DialogTitle, DialogContent, TextField, Button } from '@material-ui/core';
-import {Formik, Field, Form} from 'formik';
+import { makeStyles } from '@material-ui/core/styles';
+import {
+  Dialog,
+  DialogActions,
+  DialogTitle,
+  DialogContent,
+  Button,
+  ButtonBase,
+  Typography,
+  CardMedia,
+  LinearProgress,
+} from '@material-ui/core';
+import { Formik, Field, Form } from 'formik';
+import { TextField } from 'formik-material-ui';
 
-const styles = style => ({
-  root: {
-    flexGrow: 1,
-  },
-  deleteButton: {
-    marginLeft: 'auto',
-    marginRight: 'auto',
-    color: '#ffffff',
-    backgroundColor: '#dd7d1b',
-  },
-  editButton: {
+const useStyles = makeStyles({
+  submitButton: {
     marginLeft: 'auto',
     marginRight: 'auto',
     color: '#ffffff',
     backgroundColor: '#007398',
   },
-  buttonGroup: {
-    marginTop: '10px',
-  },
 });
 
-class EditableEvent extends React.Component {
-  constructor (props) {
-    super(props);
-    this.state = {
-      id: props.event.id,
-      name: props.event.name,
-      description: props.event.description,
-      image: props.event.image,
-      locationName: props.event.location,
-      address: {
-        line1: null,
-        line2: null,
-        city: null,
-        state: null,
-        zip: null,
-      },
-      movements: props.event.movements,
-      start: props.event.start,
-      end: props.event.end,
-      url: props.event.url,
-    };
-    this.open = props.open;
-    console.log(this.open);
+function EditableEvent(props) {
+  const classes = useStyles();
+  const { onClose, event, ...other } = props;
+  const start = moment.unix(event.start);
+  const end = moment.unix(event.end);
+  console.log(event);
+
+  function handleClose() {
+    onClose();
   }
 
-  render() {
-    const { classes } = this.props;
-    const event = this.state;
+  return (
+    <Dialog
+      disableBackdropClick
+      disableEscapeKeyDown
+      maxWidth="xl"
+      onClose={handleClose}
+      {...other}
+    >
+      <DialogTitle onClose={handleClose}>
+        Edit Event
+      </DialogTitle>
+      <DialogContent>
 
-    let start = moment.unix(this.state.start);
-    let end = moment.unix(this.state.end);
-    return (
-      <Dialog
-        disableBackdropClick
-        disableEscapeKeyDown
-        open={this.open}
-      >
-        <DialogTitle onClose={this.handleCancel}>
-          Edit Event
-        </DialogTitle>
-        <DialogContent>
-
-          {// TODO: show image as button with overlayed edit icon
-            /* <ButtonBase
-            focusRipple
-            key={image.title}
-            className={classes.image}
-            focusVisibleClassName={classes.focusVisible}
-          >
-            <span
-              className={classes.imageSrc}
-              style={{
-                backgroundImage: `url(${image.url})`,
-              }}
-            />
-            <span className={classes.imageBackdrop} />
-            <span className={classes.imageButton}>
-              <Typography
-                component="span"
-                variant="subtitle1"
-                color="inherit"
-                className={classes.imageTitle}
+        {/* TODO: show image as button with overlayed edit icon */}
+        {/* <ButtonBase
+          focusRipple
+          key={`${event.id}-image`}
+          className={classes.image}
+          focusVisibleClassName={classes.focusVisible}
+        >
+          <span
+            className={classes.imageSrc}
+            style={{
+              backgroundImage: `url(${event.imageUrl})`,
+            }}
+          />
+          <span className={classes.imageBackdrop} />
+          <span className={classes.imageButton}>
+            <Typography
+              component="span"
+              variant="subtitle1"
+              color="inherit"
+              className={classes.imageTitle}
+            >
+              {event.name}
+              <span className={classes.imageMarked} />
+            </Typography>
+          </span>
+        </ButtonBase> */}
+        <CardMedia
+          // className={classes.media}
+          component="img"
+          src={event.image || '/static/event.png'}
+          title={event.name}
+        />
+        <Formik
+          initialValues={event}
+          validate={(values) => {
+            const errors = {};
+            if (!values.email) {
+              errors.email = 'Required';
+            } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+              errors.email = 'Invalid email address';
+            }
+            return errors;
+          }}
+          onSubmit={(values, { setSubmitting }) => {
+            setTimeout(() => {
+              setSubmitting(false);
+              alert(JSON.stringify(values, null, 2));
+            }, 500);
+          }}
+          render={({
+            submitForm, isSubmitting, values, setFieldValue,
+          }) => (
+            <Form>
+              <Field
+                required
+                id="name"
+                name="name"
+                label="Event Name"
+                value={event.name}
+                margin="normal"
+                fullWidth
+                component={TextField}
+              />
+              <br />
+              <Field
+                required
+                id="description"
+                name="description"
+                label="Event description"
+                value={event.description}
+                margin="normal"
+                fullWidth
+                multiline
+                component={TextField}
+              />
+              <br />
+              {isSubmitting && <LinearProgress />}
+              <br />
+              <Button
+                variant="contained"
+                color="primary"
+                disabled={isSubmitting}
+                onClick={submitForm}
+                className={classes.submitButton}
               >
-                {image.title}
-                <span className={classes.imageMarked} />
-              </Typography>
-            </span>
-          </ButtonBase> */}
-          <Formik
-            initialValues={this.state}
-            validate={values => {
-              const errors = {};
-              if (!values.email) {
-                errors.email = 'Required';
-              } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
-                errors.email = 'Invalid email address';
-              }
-              return errors;
-            }}
-            onSubmit={(values, {setSubmitting}) => {
-              setTimeout(() => {
-                setSubmitting(false);
-                alert(JSON.stringify(values, null, 2));
-              }, 500);
-            }}
-            render={({submitForm, isSubmitting, values, setFieldValue}) => (
-              <Form>
-                <Field
-                  name="email"
-                  type="email"
-                  label="Email"
-                  component={UppercasingTextField}
-                />
-                <br />
-                <Field
-                  type="password"
-                  label="Password"
-                  name="password"
-                  component={TextField}
-                />
-                <br />
-                <FormControlLabel
-                  control={
-                    <Field label="Remember Me" name="rememberMe" component={Switch} />
-                  }
-                  label="Remember Me"
-                />
-                <br />
-                <Field
-                  type="text"
-                  name="select"
-                  label="With Select"
-                  select
-                  helperText="Please select Range"
-                  margin="normal"
-                  component={TextField}
-                  InputLabelProps={{
-                    shrink: true,
-                  }}
-                >
-                  {ranges.map(option => (
-                    <MenuItem key={option.value} value={option.value}>
-                      {option.label}
-                    </MenuItem>
-                  ))}
-                </Field>
-                <br />
-                <FormControl>
-                  <InputLabel shrink={true} htmlFor="tags">
-                    Tags
-                  </InputLabel>
-                  <Field
-                    type="text"
-                    name="tags"
-                    component={Select}
-                    multiple={true}
-                    inputProps={{name: 'tags', id: 'tags'}}
-                  >
-                    <MenuItem value="dogs">Dogs</MenuItem>
-                    <MenuItem value="cats">Cats</MenuItem>
-                    <MenuItem value="rats">Rats</MenuItem>
-                    <MenuItem value="snakes">Snakes</MenuItem>
-                  </Field>
-                </FormControl>
-                <br />
-                {isSubmitting && <LinearProgress />}
-                <br />
-                <Button
-                  variant="contained"
-                  color="primary"
-                  disabled={isSubmitting}
-                  onClick={submitForm}
-                >
-                  Submit
-                </Button>
-              </Form>
-            )}
-          />
-          <TextField
-            required
-            id="name"
-            label="Event Name"
-            defaultValue={event.name}
-            margin="normal"
-          />
-          <TextField
-            required
-            id="name"
-            label="Event Name"
-            defaultValue={event.name}
-            margin="normal"
-          />
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={this.handleCancel} color="secondary">
-              Cancel
-          </Button>
-          <Button onClick={this.handleSave} color="primary">
-              Save
-          </Button>
-        </DialogActions>
-      </Dialog>
-    );
-  }
+                Submit
+              </Button>
+            </Form>
+          )}
+        />
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={handleClose} color="secondary">
+            Cancel
+        </Button>
+        {/* <Button onClick={this.handleSave} color="primary">
+            Save
+        </Button> */}
+      </DialogActions>
+    </Dialog>
+  );
 }
 
-export default withStyles(styles)(EditableEvent);
+// TODO: possibly add more specific validation functions
+EditableEvent.propTypes = {
+  onClose: PropTypes.func.isRequired,
+  event: PropTypes.exact({
+    id: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+    description: PropTypes.string.isRequired,
+    imageUrl: PropTypes.string.isRequired,
+    locationName: PropTypes.string.isRequired,
+    address: {
+      line1: PropTypes.string.isRequired,
+      line2: PropTypes.string.isRequired,
+      city: PropTypes.string.isRequired,
+      state: PropTypes.string.isRequired,
+      zip: PropTypes.string.isRequired,
+    },
+    movements: PropTypes.array.isRequired,
+    start: PropTypes.string.isRequired,
+    end: PropTypes.string.isRequired,
+    url: PropTypes.string.isRequired,
+  }).isRequired,
+  open: PropTypes.bool.isRequired,
+};
+
+export default EditableEvent;
