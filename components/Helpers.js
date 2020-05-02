2ade03e539e0692fdf10f8ba8e93c759c6a64f6c
Jinchi Zhou <jinchizhou@yahoo.com>
2020-05-02 16:10:52 -0700

    func finally works, when i had getUsersById as separate, function was not waiting for firestore to come back

--- components/Helpers.js
+++ components/Helpers.js
@@ -1,18 +1,23 @@
 import { db } from '../src/firebase/firebaseSetup.js';
 
-export function getUserNameById (id) {
-    var docRef = db.collection("users").doc(id);
-    console.log("Doc ref: ", docRef);
-    docRef.get().then(function(doc) {
+export function getUsers (list) {
+    let users = [];
+    for (let i = 0; i < list.length; i++) {
+      console.log("id: ", list[i].id);
+      db.collection("users").doc(list[i].id).get().then(function(doc) {
         if (doc.exists) {
-            console.log("Document data:", doc.data());
-            return doc.data();
+            // console.log("got data ", doc.data());
+            var data = doc.data();
+            var name = data.name.first + " " + data.name.last;
+            users.push(name);
         } else {
             // doc.data() will be undefined in this case
             console.log("No such document!");
         }
-    }).catch(function(error) {
-        console.log("Error getting document:", error);
-    });
-    return "Invalid User";
+      }).catch(function(error) {
+          console.log("Error getting document:", error);
+      });
+    }
+    return users;
 };
+
