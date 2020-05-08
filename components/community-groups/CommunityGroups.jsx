0789e2a985edfe3da54a43927fcac577b9e125f7
Jinchi Zhou <jinchizhou@yahoo.com>
2020-05-08 00:04:49 -0700

    migrated data

--- components/community-groups/CommunityGroups.jsx
+++ components/community-groups/CommunityGroups.jsx
@@ -106,59 +106,6 @@ class CommunityGroups extends React.Component {
     return Promise.all(promises);
   }
 
-  // getBoth = () => {
-  //   var cgs = db.collection('communitygroups').get().then(
-  //     (querySnapshot) => {
-  //       var promises = [];
-  //       querySnapshot.forEach((doc) => {
-  //         let cg = doc.data();
-  //         promises.push([doc.id, cg]);
-  //       });
-  //       return promises;
-  //     }
-  //   );
-  //   var users = db.collection('users').get().then(
-  //     (querySnapshot) => {
-  //       var promises = [];
-  //       querySnapshot.forEach((doc) => {
-  //         // console.log("id ", doc.id);
-  //         let cg = doc.data();
-  //         promises.push([doc.id, cg]);
-  //       });
-  //       return promises;
-  //     }
-  //   );
-
-  //   Promise.all([cgs, users]).then((data) => {
-  //     let cgs = data[0];
-  //     let users = data[1];
-  //     // console.log("cgs are: ", cgs);
-  //     // console.log("users are: ", users);
-  //     let userMap = {};
-  //     // iterate through all users and map id to name
-  //     users.forEach((user) => {
-  //       let id = user[0];
-  //       console.log("id ", id);
-  //       let name = user[1].name.first + " " + user[1].name.last;
-  //       userMap[id] = name;
-  //       console.log("name ", name);
-  //     });
-  //     var cgRef = db.collection("communitygroups");
-  //     cgs.forEach((cg) => {
-  //       let leaders = cg[1].leaders;
-  //       var leadersNames = [];
-  //       leaders.forEach((leader) => {
-  //         console.log("leader id ", leader.id);
-  //         console.log("Name of leader ", userMap[leader.id]);
-  //         leadersNames.push(userMap[leader.id]);
-  //       });
-  //       cgRef.doc(cg[0]).set({
-  //         leadersNames
-  //       }, {merge: true});
-  //     }); 
-  //   });
-  // }
-
   getCommunityGroups = () => {
     let cgs = [];
     // this should be a promise?
@@ -180,15 +127,6 @@ class CommunityGroups extends React.Component {
   // filter based on list of [fieldName, fieldValue)]
   filterWithOptions = (options, cgs) => {
     console.log("Options: ", options);
-    // let filters = [];
-    // for (let i = 0; i < options.length; i++) {
-    //   if (options[i][1] !== "All"){
-    //     filters.push(options[i]);
-    //   }
-    // }
-    // if (filters.length === 0) {
-    //   return cgs;
-    // }
     let filteredData = cgs;
     // filter by each option 
     for (let i = 0; i < options.length; i++) {
