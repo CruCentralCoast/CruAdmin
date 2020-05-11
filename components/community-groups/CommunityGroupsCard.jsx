97b14bb5237204224dcb69a3f0b21a43c5d145a5
Jinchi Zhou <jinchizhou@yahoo.com>
2020-05-10 23:45:34 -0700

    button works, form is empty

--- components/community-groups/CommunityGroupsCard.jsx
+++ components/community-groups/CommunityGroupsCard.jsx
@@ -2,18 +2,21 @@ import * as React from 'react';
 import PropTypes from 'prop-types';
 import { makeStyles } from '@material-ui/core/styles';
 import {
-  Card, CardMedia, CardContent, Typography, Button, Grid,
+  Card, CardActions, CardContent, Typography, Button, Grid,
 } from '@material-ui/core';
-import { db } from '../../src/firebase/firebaseSetup.js';
-import { inspect } from 'util';
+import TextField from '@material-ui/core/TextField';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
 // EditableCG here
 // import EditableEvent from '../editableEvent/EditableEvent';
 import CommunityGroupModel from '../../src/models/CommunityGroup';
-import { getUsers } from '../Helpers';
 
 const useStyles = makeStyles({
   root: {
-    flexGrow: 1,
+    minWidth: 250,
   },
   deleteButton: {
     marginLeft: 'auto',
@@ -28,8 +31,13 @@ const useStyles = makeStyles({
     backgroundColor: '#007398',
   },
   buttonGroup: {
-    marginTop: '10px',
+    alignItems: 'center'
   },
+  cardControl: {
+    width: '18vw',
+    height: '20vw',
+    minWidth: 275,
+  }
 });
 
 export default function CommunityGroupsCard(props) {
@@ -38,61 +46,71 @@ export default function CommunityGroupsCard(props) {
   // const [selectedValue, setSelectedValue] = React.useState(emails[1]);
   const { cg } = props;
 
-  console.log("leaders: " + cg.leaders);
-  // let leaders = [];
-  // getUsers(cg.leaders).then(function(result) {
-  //   console.log("leaders from card is: ", result);
-  // });
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
 
-//   function handleClickOpen() {
-//     setOpen(true);
-//   }
-
-//   const handleClose = () => {
-//     setOpen(false);
-//     // setSelectedValue(value);
-//   };
-
-//   const now = moment();
-//   const editButton = (<Button key={`${cg.id}-edit`} className={classes.editButton} onClick={handleClickOpen}>Edit</Button>);
-//   const deleteButton = (<Button key={`${cg.id}-delete`} className={classes.deleteButton}>Delete</Button>);
-//   let buttons;
-
-//   if (event.end > now) {
-//     buttons = [
-//       editButton,
-//       deleteButton,
-//     ];
-//   } else if (now.isAfter(event.start) && now.isBefore(event.end)) {
-//     // An event should not be able to be deleted while it is in progress
-//     buttons = editButton;
-//   } else {
-//     // The only option for events that have ended should be deletion
-//     buttons = deleteButton;
-//   }
+  const handleClose = () => {
+    setOpen(false);
+  };
   
-  // TODO: partition by year and filter by movement
   // default to filtered to movement CRU
   return (
-    <Card>
-      <CardContent>
-        <Typography gutterBottom variant="h5" component="h2">
-          {cg.year}
-        </Typography>
-        <Typography component="p">
-          {cg.gender}
-        </Typography>
-        <Typography component="p">
-          {"Leaders: " + (cg.leadersNames || "TBD")}
-        </Typography>
-        <Typography component="p">
-          {"Meets on: " + (cg.day || "TBD")}
-        </Typography>
-        <Typography component="p">
-          {"Location: " + (cg.dorm || "TBD")}
-        </Typography>
-      </CardContent>
-    </Card>
+    <div>
+      <Card className={classes.cardControl}>
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="h2">
+            {cg.year}
+          </Typography>
+          <Typography component="p">
+            {cg.gender}
+          </Typography>
+          <Typography component="p">
+            {"Leaders: " + (cg.leadersNames || "TBD")}
+          </Typography>
+          <Typography component="p">
+            {"Meets on: " + (cg.day || "TBD")}
+          </Typography>
+          <Typography component="p">
+            {"Location: " + (cg.dorm || "TBD")}
+          </Typography>
+        </CardContent>
+        <div className={classes.buttonGroup}>
+          <Button variant="outlined" color="primary" onClick={handleClickOpen}>
+            Edit CG
+          </Button>
+          <Button variant="outlined" color="secondary" onClick={handleClickOpen}>
+            Delete CG
+          </Button>
+        </div>
+      </Card>
+      <Dialog open={open} onClose={handleClose} aria-labelledby="form-dialog-title">
+        <DialogTitle id="form-dialog-title">Subscribe</DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            To subscribe to this website, please enter your email address here. We will send updates
+            occasionally.
+          </DialogContentText>
+          <TextField
+            autoFocus
+            margin="dense"
+            id="name"
+            label="Email Address"
+            type="email"
+            fullWidth
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose} color="primary">
+            Cancel
+          </Button>
+          <Button onClick={handleClose} color="primary">
+            Subscribe
+          </Button>
+        </DialogActions>
+      </Dialog>
+
+    </div>
   );
 }
 
