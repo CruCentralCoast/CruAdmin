87c0056ebe1cd426c481844a0aec4201f3156113
Jinchi Zhou <jinchizhou@yahoo.com>
2020-05-13 23:32:03 -0700

    selectChanges work

--- components/community-groups/CommunityGroupsCard.jsx
+++ components/community-groups/CommunityGroupsCard.jsx
@@ -13,7 +13,7 @@ import DialogTitle from '@material-ui/core/DialogTitle';
 import NativeSelect from '@material-ui/core/NativeSelect';
 // EditableCG here
 // import EditableEvent from '../editableEvent/EditableEvent';
-import CommunityGroupModel from '../../src/models/CommunityGroup';
+import { generateOptions } from '../Helpers';
 
 const useStyles = makeStyles({
   root: {
@@ -45,7 +45,6 @@ const useStyles = makeStyles({
   }
 });
 
-// generate String to display group of leaders
 const generateStringOfGroup = (group) => {
   var str = "";
   str += group[0];
@@ -55,11 +54,111 @@ const generateStringOfGroup = (group) => {
   return str;
 }
 
+const generateOptionsByNames = (users) => {
+  let l = [];
+  for (let i = 0; i < users.length; i++) {
+    let name = users[i].name.first + " " + users[i].name.last;
+    l.push(<option value={name}>{name}</option>);
+  }
+  return l;
+}
+
+// const buildLeaderSelects = (names, options) => {
+//   let l = [];
+//   for (let i = 0; i < names.length; i++) {
+//     let name = users[i].name.first + " " + users[i].name.last;
+//     l.push(
+//       <div>
+//         <InputLabel>Leaders</InputLabel>
+//         <NativeSelect
+//           value={name}
+//           onChange={handleCloseEdit}
+//           name='leadersNames'
+//         >
+//         {leaderOptions}
+//         </NativeSelect>
+//       </div>
+//       );
+//   }
+//   return l;
+// }
+
 export default function CommunityGroupsCard(props) {
   const classes = useStyles();
+
+  const [openEdit, setOpenEdit] = React.useState(false);
+  const [openRem, setOpenRem] = React.useState(false);
   // const [selectedValue, setSelectedValue] = React.useState(emails[1]);
-  const { cg } = props;
+  const { cg, users } = props;
+
+  const [cgFinal, setCgFinal] = React.useState({
+    year: cg.year,
+    leadersNames: cg.leadersNames,
+    gender: cg.gender,
+    day: cg.day,
+    dorm: cg.dorm
+  });
+
+  const leaderOptions = generateOptionsByNames(users);
+  const years = ["Freshman", "Sophomore", "Junior", "Senior"];
+  const yearOptions = generateOptions(years);
+  const gender = ["Male", "Female"];
+  const genderOptions = generateOptions(gender);
+  console.log("Year options are: ", yearOptions);
+
   cg.leadersNamesString = generateStringOfGroup(cg.leadersNames);
+
+  const handleClickOpenEdit = () => {
+    setOpenEdit(true);
+  };
+
+  const handleCloseEdit = () => {
+    setOpenEdit(false);
+  };
+
+  const handleClickOpenRem = () => {
+    setOpenRem(true);
+  };
+
+  const handleCloseRem = (remove) => {
+    console.log("Remove CG ", remove);
+    setOpenRem(false);
+  };
+
+  const indexSelectChange = (event, index) => {
+    const value = event.target.value;
+    var cgLeaders = cgFinal.leadersNames;
+    cgLeaders[index] = value;
+    setCgFinal({
+      ...cgFinal, // necessary merging existing state with new state
+      leadersNames: cgLeaders
+    });
+  }
+
+  const selectChange = (event) => {
+    const name = event.target.name;
+    const value = event.target.value;
+    console.log("Name change is: ", name);
+    console.log("Value change is: ", value);
+    setCgFinal({
+      ...cgFinal, // necessary merging existing state with new state
+      [name]: value
+    });
+  }
+  
+  const leaderSelects = cg.leadersNames.map((name, index) => {
+    return (<div>
+      <InputLabel>Leaders</InputLabel>
+      <NativeSelect
+        value={name}
+        onChange={(event) => indexSelectChange(event, index)}
+        name={index}
+      >
+      {leaderOptions}
+      </NativeSelect>
+    </div> 
+    );
+  });
   
   // default to filtered to movement CRU
   return (
@@ -82,7 +181,91 @@ export default function CommunityGroupsCard(props) {
             {"Location: " + (cg.dorm || "TBD")}
           </Typography>
         </CardContent>
+        <div className={classes.buttonGroup}>
+          <Button variant="outlined" color="primary" onClick={handleClickOpenEdit}>
+            Edit CG
+          </Button>
+          <Button variant="outlined" color="secondary" onClick={handleClickOpenRem}>
+            Remove CG
+          </Button>
+        </div>
       </Card>
+      <Dialog open={openEdit} onClose={handleCloseEdit} aria-labelledby="form-dialog-title">
+        <DialogTitle id="form-dialog-title">Edit CG</DialogTitle>
+        <DialogContent>
+          <div>
+            <FormControl className={classes.formControl}>
+              <InputLabel>Year</InputLabel>
+              <NativeSelect
+                value={cgFinal.year}
+                onChange={selectChange}
+                name='year'
+              >
+                {yearOptions}
+              </NativeSelect>
+            </FormControl>
+          </div>
+          
+          <br/>
+          <div>
+            <FormControl className={classes.formControl}>
+              <InputLabel>Gender</InputLabel>
+              <NativeSelect
+                value={cgFinal.gender}
+                onChange={selectChange}
+                name='gender'
+              >
+                {genderOptions}
+              </NativeSelect>
+            </FormControl>
+          </div>
+          
+          <br/>
+          <FormControl className={classes.formControl}>
+            {leaderSelects}
+          </FormControl>
+          <br/>
+          <TextField
+              autoFocus
+              margin="dense"
+              id="name"
+              label="Meets at"
+              type="email"
+              fullWidth
+            />
+          <br/>
+        </DialogContent>
+          
+        <DialogActions>
+          <Button onClick={handleCloseEdit} color="primary">
+            Cancel
+          </Button>
+          <Button onClick={handleCloseEdit} color="primary">
+            Subscribe
+          </Button>
+        </DialogActions>
+      </Dialog>
+
+      <Dialog open={openRem} onClose={() => handleCloseRem(false)} aria-labelledby="form-dialog-title">
+        <DialogTitle id="form-dialog-title">Remove CG</DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            Are you sure you would like to permanently remove CG?
+            <br />
+            Led by {cg.leadersNamesString}
+          </DialogContentText>
+
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={() => handleCloseRem(true)} color="primary">
+            Yes
+          </Button>
+          <Button onClick={() => handleCloseRem(false)} color="primary">
+            No
+          </Button>
+        </DialogActions>
+      </Dialog>
+
     </div>
   );
 }
