6eee45324f8114dbf255c3a6be128d1a6af37754
Jinchi Zhou <jinchizhou@yahoo.com>
2020-05-13 23:32:03 -0700

    selectChanges work

--- components/community-groups/CommunityGroupsCard.jsx
+++ components/community-groups/CommunityGroupsCard.jsx
@@ -1,10 +1,17 @@
 import * as React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import {
-  Card, CardContent, Typography
+  Card, CardContent, Typography, Button
 } from '@material-ui/core';
 
+import { db } from '../../src/firebase/firebaseSetup.js';
+import EditForm from './CommunityGroupsEditForm';
+import RemoveForm from '../form/RemoveForm';
+
 const useStyles = makeStyles({
+  buttonGroup: {
+    alignItems: 'center'
+  },
   cardControl: {
     width: '18vw',
     height: '20vw',
@@ -24,31 +31,72 @@ const stringifyLeaderNames = (group) => {
 
 export default function CommunityGroupsCard(props) {
   const classes = useStyles();
-  const { cg } = props;
-  cg.leadersNamesString = stringifyLeaderNames(cg.leadersNames);
+  const { cg, users, removeCallback } = props;
+
+  // form handlers
+  const [openEdit, setOpenEdit] = React.useState(false);
+  const [openRem, setOpenRem] = React.useState(false);
+
+  // stores up to date values of cg
+  const [currCG, setCurrCG] = React.useState(cg);
+
+  currCG.leadersNamesString = stringifyLeaderNames(currCG.leadersNames);
+
+  const handleEdit = (edit) => {
+    setOpenEdit(edit);
+  };
+
+  const handleRem = (remove) => {
+    setOpenRem(remove);
+  };
+
+  const updateCG = (cg) => {
+    db.collection("communitygroups").doc(cg.id).update({
+      day: cg.day,
+      dorm: cg.dorm,
+      gender: cg.gender,
+      leadersNames: cg.leadersNames,
+      year: cg.year
+    }).then(() => {
+      setCurrCG(cg);
+    });
+  }
   
-  // default to filtered to movement CRU
+  // display single CG Card (including edit/remove form)
   return (
     <div>
       <Card className={classes.cardControl}>
         <CardContent>
           <Typography gutterBottom variant="h5" component="h2">
-            {cg.year}
+            {currCG.year}
           </Typography>
           <Typography component="p">
-            {cg.gender}
+            {currCG.gender}
           </Typography>
           <Typography component="p">
-            {"Leaders: " + (cg.leadersNamesString || "TBD")}
+            {"Leaders: " + (currCG.leadersNamesString || "TBD")}
           </Typography>
           <Typography component="p">
-            {"Meets on: " + (cg.day || "TBD")}
+            {"Meets on: " + (currCG.day || "TBD")}
           </Typography>
           <Typography component="p">
-            {"Location: " + (cg.dorm || "TBD")}
+            {"Location: " + (currCG.dorm || "TBD")}
           </Typography>
         </CardContent>
+        <div className={classes.buttonGroup}>
+          <Button variant="outlined" color="primary" onClick={handleEdit}>
+            Edit CG
+          </Button>
+          <Button variant="outlined" color="secondary" onClick={() => handleRem(true)}>
+            Remove CG
+          </Button>
+        </div>
       </Card>
+      <EditForm open={openEdit} cg={currCG} 
+      users={users} handleEdit={handleEdit} updateCG={updateCG}>
+      </EditForm>
+      <RemoveForm open={openRem} cg={currCG} handleRem={handleRem} removeCallback={removeCallback}>
+      </RemoveForm>
     </div>
   );
 }
