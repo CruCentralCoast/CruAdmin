6803508e32fd6a733207f9dafbefe1035cba51d1
Jinchi Zhou <jinchizhou@yahoo.com>
2020-05-16 19:09:28 -0700

    form edit component looks pretty good

--- components/community-groups/CommunityGroupsCard.jsx
+++ components/community-groups/CommunityGroupsCard.jsx
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import React, { useState, useEffect } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import {
   Card, CardContent, Typography, Button
