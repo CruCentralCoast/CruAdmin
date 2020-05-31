ce72ff0235ec2f1d19b08749d7ea313dd5d06ce7
Jinchi Zhou <jinchizhou@yahoo.com>
2020-05-30 23:41:17 -0700

    finished data dup for ministry teams

--- scripts/data-dup.js
+++ scripts/data-dup.js
@@ -3,56 +3,45 @@
  They are meant to ease data duplication for 
  collections where we may need it. These are ran on the spot.
  */
- // get 2 collections, update data in one collection
- function dataDup() {
-    var cgs = db.collection('communitygroups').get().then(
-      (querySnapshot) => {
-        var promises = [];
-        querySnapshot.forEach((doc) => {
-          let cg = doc.data();
-          promises.push([doc.id, cg]);
-        });
-        return promises;
-      }
-    );
-    var users = db.collection('users').get().then(
-      (querySnapshot) => {
-        var promises = [];
-        querySnapshot.forEach((doc) => {
-          // console.log("id ", doc.id);
-          let cg = doc.data();
-          promises.push([doc.id, cg]);
-        });
-        return promises;
-      }
-    );
+import { getAllFromFirestore } from '../components/Helpers';
+import { db } from '../src/firebase/firebaseSetup.js';
 
-    Promise.all([cgs, users]).then((data) => {
-      let cgs = data[0];
+ // get 2 collections, update data in one collection
+ export function dataDup() {
+  var mts = getAllFromFirestore('ministryteams');
+  var users = getAllFromFirestore('users');
+  var mvts = getAllFromFirestore('ministryteams');
+
+  // get all mts, users
+  Promise.all([mts, users]).then((data) => {
+      let mts = data[0];
       let users = data[1];
-      // console.log("cgs are: ", cgs);
-      // console.log("users are: ", users);
+      console.log("mts are: ", mts);
+      console.log("users are: ", users);
       let userMap = {};
       // iterate through all users and map id to name
       users.forEach((user) => {
-        let id = user[0];
-        // console.log("id ", id);
-        let name = user[1].name.first + " " + user[1].name.last;
-        userMap[id] = name;
-        // console.log("name ", name);
+          console.log(user);
+          let id = user.id;
+          console.log("id ", id);
+          let name = user.name.first + " " + user.name.last;
+          userMap[id] = name;
+          console.log("name ", name);
       });
-      var cgRef = db.collection("communitygroups");
-      cgs.forEach((cg) => {
-        let leaders = cg[1].leaders;
-        var leadersNames = [];
-        leaders.forEach((leader) => {
-          // console.log("leader id ", leader.id);
-          // console.log("Name of leader ", userMap[leader.id]);
-          leadersNames.push(userMap[leader.id]);
-        });
-        cgRef.doc(cg[0]).set({
+      var mtsRef = db.collection("ministryteams");
+      mts.forEach((cg) => {
+          console.log("cg is ", cg);
+          let leaders = cg.leaders;
+          var leadersNames = [];
+          leaders.forEach((leader) => {
+              console.log("leader id ", leader.id);
+              console.log("Name of leader ", userMap[leader.id]);
+              leadersNames.push(userMap[leader.id]);
+          });
+          // add leadersNames
+          mtsRef.doc(cg.id).set({
           leadersNames
-        }, {merge: true});
+          }, {merge: true});
       }); 
-    });
+  });
   }
