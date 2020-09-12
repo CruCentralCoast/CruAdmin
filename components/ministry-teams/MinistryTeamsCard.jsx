f6637eece4a6a89046f993bdcbb3c1f122b08369
Jinchi Zhou <jinchizhou@yahoo.com>
2020-09-12 16:24:35 -0700

    edit and add looks pretyt good

--- components/ministry-teams/MinistryTeamsCard.jsx
+++ components/ministry-teams/MinistryTeamsCard.jsx
@@ -1,11 +1,18 @@
 import * as React from 'react';
-import { Card, CardContent, CardMedia, makeStyles, Typography
+import {
+  Button, Card, CardContent, CardMedia, makeStyles, Typography
 } from '@material-ui/core';
+
+import { db, storage } from '../../src/firebase/firebaseSetup.js';
 
 import { stringifyLeaderNames } from '../Helpers';
 import { DESC_LIMIT } from '../constants';
+import EditForm from './MinistryTeamsEditForm';
 
 const useStyles = makeStyles({
+  buttonGroup: {
+    alignItems: 'center'
+  },
   cardControl: {
     width: '18vw',
     minWidth: 250,
@@ -16,10 +23,13 @@ const useStyles = makeStyles({
 // Represents a single card of Ministry Team
 export default function MinistryTeamsCard(props) {
   const classes = useStyles();
-  const { mt } = props;
+  const { mt, users } = props;
+
+  // form handlers
+  const [openEdit, setOpenEdit] = React.useState(false);
 
   // stores up to date values of Ministry Team
-  const [currMT] = React.useState(mt);
+  const [currMT, setCurrMT] = React.useState(mt);
 
   const truncString = (str) => {
     if (str.length > DESC_LIMIT) {
@@ -29,6 +39,66 @@ export default function MinistryTeamsCard(props) {
   }
   currMT.leadersNamesString = stringifyLeaderNames(currMT.leadersNames);
 
+  const handleEdit = (edit) => {
+    setOpenEdit(edit);
+  };
+
+  const updateMTInFirebase = (mt, url) => {
+    db.collection("ministryteams").doc(mt.id).update({
+      description: mt.description,
+      imageLink: url,
+      leadersNames: mt.leadersNames,
+      name: mt.name
+    }).then(() => {
+      // id needed for Firestore, imageLink retrieves image
+      console.log("new mt is: ", mt);
+      mt.imageLink = url;
+      setCurrMT(mt);
+    });
+  }
+
+  const updateMT = (mt) => {
+    // try to update MT
+
+    console.log("Update MT");
+    console.log("mt is ", mt);
+    // update new pic
+    if (mt.pic) {
+      // ref to image
+      const imageRef = storage.ref().child(mt.pic.name);
+
+      // if image already exists, reuse url
+      imageRef.getDownloadURL().then((foundURL) => {
+        updateMTInFirebase(mt, foundURL);
+      }, () => {
+        // since image doesn't exist, upload image
+        console.warn("File ", mt.pic.name, " doesn't exist");
+        const uploadTask = imageRef.put(mt.pic);
+        // check on status of upload task
+        uploadTask.on(
+          "state_changed",
+          snapshot => {},
+          error => {
+            console.log(error);
+          },
+          () => {
+            storage
+              .ref()
+              .child(mt.pic.name)
+              .getDownloadURL()
+              .then(url => {
+                updateMTInFirebase(mt, url);
+              });
+          }
+        )
+      });
+    } else {
+      updateMTInFirebase(mt, mt.imageLink)
+    }
+    
+  }
+
+  // display a single MT Card (including edit/remove form)
   return (
     <div>
       <Card className={classes.cardControl}>
@@ -48,7 +118,18 @@ export default function MinistryTeamsCard(props) {
             {"Description: " + (truncString(currMT.description) || "TBD")}
           </Typography>
         </CardContent>
+        <div className={classes.buttonGroup}>
+          <Button variant="outlined" color="primary" onClick={handleEdit}>
+            Edit MT
+          </Button>
+          <Button variant="outlined" color="secondary" onClick={() => handleRemove(true)}>
+            Remove MT
+          </Button>
+        </div>
       </Card>
+      <EditForm open={openEdit} mt={currMT} update="true"
+        users={users} handleEdit={handleEdit} updateMT={updateMT}>
+      </EditForm>
     </div>
   );
 }
