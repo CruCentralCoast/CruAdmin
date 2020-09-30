129af4ada341a38e531f859e50cc4850ffba669e
Jinchi Zhou <jinchizhou@gmail.com>
2020-09-29 23:37:25 -0700

    displaying time

--- components/missions/MissionCard.jsx
+++ components/missions/MissionCard.jsx
@@ -17,7 +17,8 @@ export default function MissionCard(props) {
 
   // stores up to date values of Mission
   const [currMission] = React.useState(mission);
-
+  console.log("date is ", currMission.startDate);
+  // display time and location
   return (
     <div>
       <Card className={classes.cardControl}>
@@ -29,6 +30,15 @@ export default function MissionCard(props) {
         <CardContent>
           <Typography gutterBottom variant="h5" component="h2">
             {currMission.name}
+          </Typography>
+          <Typography component="p">
+            {`Location: ${currMission.location}`}
+          </Typography>
+          <Typography component="p">
+            {`Start Date: ${new Date(currMission.startDate.seconds).toLocaleString()}`}
+          </Typography>
+          <Typography component="p">
+            {`End Date: ${new Date(currMission.endDate.seconds).toLocaleString()}`}
           </Typography>
           <Link href={currMission.url} onClick={(event) => {event.preventDefault(); window.open(currMission.url);}}>
             Click for more info
